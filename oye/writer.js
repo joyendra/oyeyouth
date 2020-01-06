3804b21677ee85a398084f1cfe7ce0ab102213a1
Joyendra Roy Biswas <36339255+joyendra@users.noreply.github.com>
2020-01-06 19:39:51 +0530

    Added Website basic structure
    basic frame

---
+++ oye/writer.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    var typed = new Typed('#typed', {
+
+        strings: ['<strong>Organization for Youth Empowerment</strong>', '<strong>a volunteer run youth initiative</strong>', '<strong>an attempt to empower youth</strong>', '<strong>an attempt to bring a change</strong>'],
+        typeSpeed: 60,
+        backSpeed: 100,
+        fadeOut: true,
+        loop: true,
+        showCursor: false
+    });
+
+});
